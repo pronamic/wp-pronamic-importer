b7f79a158cc49a0c0a29d5824996aedd890de10a
Remco Tolsma <info@remcotolsma.nl>
2012-11-14 14:53:51 +0100

    WordPress Coding Standards optimizations.

--- classes/Pronamic/Importer/Importer.php
+++ classes/Pronamic/Importer/Importer.php
@@ -1,65 +1,86 @@
 <?php
 
+/**
+ * Title: Importer
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0
+ */
 class Pronamic_Importer_Importer extends WP_Importer {
+	/**
+	 * Dispatch
+	 */
 	public function dispatch() {
 		include Pronamic_Importer_Plugin::$dirname . '/admin/importer.php';
 	}
 
+	//////////////////////////////////////////////////
+
+	/**
+	 * Get default importer
+	 * 
+	 * @param PDO $pdo
+	 */
 	public static function get_default_importer( $pdo ) {
 		$importer = new Importer();
 	
-		$importer->next(new ExecuteQuery($pdo, 'UPDATE nieuws SET wordpress_import_attempts = wordpress_import_attempts + 1 WHERE nws_id = :import_id;' ) );
+		$importer->next( new ExecuteQuery( $pdo, 'UPDATE nieuws SET wordpress_import_attempts = wordpress_import_attempts + 1 WHERE nws_id = :import_id;' ) );
 	
-		$importer->next(new CreatePhpQueryFromPostContent());
+		$importer->next( new CreatePhpQueryFromPostContent() );
 	
-		$importer->next(new SetPostStatus('publish'));
+		$importer->next( new SetPostStatus( 'publish' ) );
 	
-		$importer->next(new SetMetaImportUrl('_import_url'));
+		$importer->next( new SetMetaImportUrl( '_import_url' ) );
 	
-		$importer->next(new SetDateIfNotSet());
-		$importer->next(new SetPostDate());
+		$importer->next( new SetDateIfNotSet() );
+		$importer->next( new SetPostDate() );
 	
-		$importer->next(new ConvertRelativeUrlToAbsolute('img', 'src'));
-		$importer->next(new ConvertRelativeUrlToAbsolute('a', 'href'));
-	
-		$importer->next(new SetPostContent());
+		$importer->next( new ConvertRelativeUrlToAbsolute( 'img', 'src' ) );
+		$importer->next( new ConvertRelativeUrlToAbsolute( 'a', 'href' ) );
+
+		$importer->next( new Pronamic_Importer_Actions_StripSlashesPostData( 'post_title' ) );
+		$importer->next( new Pronamic_Importer_Actions_StripSlashesPostData( 'post_content' ) );
 		
-		$importer->next(new RemoveElements('p:empty'));
-	
-		$importer->next(new FindPostThumbnail('this:has(img):first'));
+		$importer->next( new SetPostContent() );
 		
-		$importer->next(new FindImagesInContent());
+		$importer->next( new RemoveElements( 'p:empty' ) );
 	
-		$importer->next(new SetPostThumbnailIfNotSet());
-	
-		$importer->next(new DownloadMedia());
+		$importer->next( new FindPostThumbnail( 'this:has(img):first' ) );
 		
-		$importer->next(new InsertPost());
-		$importer->next(new InsertAttachments());
+		$importer->next( new FindImagesInContent() );
 	
-		$importer->next(new UpdateImageSource());
-		$importer->next(new UpdateFileLink());
+		$importer->next( new SetPostThumbnailIfNotSet() );
 	
-		$importer->next(new SetPostContent());
+		$importer->next( new DownloadMedia() );
 		
-		$importer->next(new TidyContent(array('css-prefix' => 'pronamic-import')));
+		$importer->next( new InsertPost() );
+		$importer->next( new InsertAttachments() );
 	
-		$importer->next(new TrimContent());
+		$importer->next( new UpdateImageSource() );
+		$importer->next( new UpdateFileLink() );
 	
-		$importer->next(new UpdatePost());
+		$importer->next( new SetPostContent() );
 		
-		$importer->next(new AddPostMeta());
-		$importer->next(new SetPostTerms());
+		$importer->next( new TidyContent( array( 'css-prefix' => 'pronamic-import' ) ) );
+	
+		$importer->next( new TrimContent() );
+	
+		$importer->next( new UpdatePost() );
 		
-		$importer->next(new InsertComments());
-	
-		$importer->next(new DeleteTemporaryFiles());
+		$importer->next( new AddPostMeta() );
+		$importer->next( new SetPostTerms() );
 		
-		$importer->next(new VarDumpImport());
+		$importer->next( new InsertComments() );
 	
-		$importer->next(new ExecuteQuery($pdo, 'UPDATE nieuws SET wordpress_imported = TRUE WHERE nws_id = :import_id;'));
+		$importer->next( new DeleteTemporaryFiles() );
+		
+		$importer->next( new VarDumpImport() );
 	
-		$importer->next(new Done());
+		$importer->next( new ExecuteQuery( $pdo, 'UPDATE nieuws SET wordpress_imported = TRUE WHERE nws_id = :import_id;' ) );
+	
+		$importer->next( new Done() );
 	
 		return $importer;
 	}

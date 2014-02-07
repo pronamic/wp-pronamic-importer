8fc2b88dbd9ecd302a0876dc0adc98408ef95099
Leon Rowland <leon@rowland.nl>
2014-02-07 10:03:37 +0100

    action to be called after wordpress update

--- classes/UpdatePost.php
+++ classes/UpdatePost.php
@@ -5,12 +5,12 @@ class UpdatePost extends ImportAction {
 		$import->log(sprintf('Updating WordPress post with <strong>%d</strong> fields &hellip', count($import->post)));
 
 		$result = wp_update_post($import->post);
-
+        
 		if($result == 0) {
 			$import->log('Failed to update post');
 		} else {
 			$import->log(sprintf('Updated WordPress post with ID %s', $result));
-	
+            do_action( 'pronamic_importer_update_post', $result, $import );
 			$this->next($import);
 		}
 	}
